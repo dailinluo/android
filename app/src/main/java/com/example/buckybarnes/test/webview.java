0eeff9d07c730f6ee2cdf1149fc92550c57a1062
dailinluo <32460791+dailinluo@users.noreply.github.com>
2017-12-01 14:16:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/buckybarnes/test/webview.java
@@ -0,0 +1,22 @@
+package com.example.buckybarnes.test;
+
+/**
+ * Created by BuckyBarnes on 2017/11/26.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class webview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        String url = "http://google.com";
+        WebView view = (WebView) this.findViewById(R.id.webview);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+    }
+}
